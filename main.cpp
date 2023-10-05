558c69656af8410d43ef16ac54eb9669abb26d99
Daniel Adámek <daniel.adamek123@icloud.dom>
2023-10-05 18:16:04 +0200

    city image

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
-#include <iostream>
-#include <opencv4/opencv2/opencv.hpp>
+#include "src/Map.h"
+#include "src/Tile.h"
+#include "src/Map.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Map myMap(20, 20, Tile::GROUND);
+    myMap.generateCities(5);
+    myMap.generatePaths();
+    myMap.printMap();
     return 0;
 }
--- src/Map.cpp
+++ src/Map.cpp
@@ -1,19 +1,27 @@
+#include <queue>
+#include <unordered_map>
+#include <map>
+#include <set>
 #include "Map.h"
 
-Map::Map(int rows, int cols, Tile defaultTile) : rows(rows), cols(cols) {
-    grid.resize(rows, std::vector<Tile>(cols, defaultTile));
+Map::Map(int rows, int cols, Tile defaultTile) : m_Rows(rows), m_Cols(cols) {
+    m_Grid.resize(rows, std::vector<Tile>(cols, defaultTile));
+
+    // Random device generator
+    std::random_device rd;
+    gen = std::mt19937(rd());
 }
 
 void Map::setTile(int x, int y, Tile tile) {
-    if (x >= 0 && x < rows && y >= 0 && y < cols) grid[x][y] = tile;
+    if (x >= 0 && x < m_Rows && y >= 0 && y < m_Cols) m_Grid[x][y] = tile;
 }
 
 Tile Map::getTile(int x, int y) const {
-    return (x >= 0 && x < rows && y >= 0 && y < cols) ? grid[x][y] : Tile::ERROR;
+    return (x >= 0 && x < m_Rows && y >= 0 && y < m_Cols) ? m_Grid[x][y] : Tile::ERROR;
 }
 
 void Map::printMap() const {
-    for (const auto& row : grid) {
+    for (const auto& row : m_Grid) {
         for (const auto& tile : row) {
             switch (tile) {
                 case Tile::GROUND:
@@ -26,7 +34,7 @@ void Map::printMap() const {
                     std::cout << '.';
                     break;
                 case Tile::PATH_CROSS:
-                    std::cout << '+';
+                    std::cout << "█";
                     break;
                 case Tile::PATH_ENDDOWN:
                     std::cout << 'V';
@@ -46,8 +54,32 @@ void Map::printMap() const {
                 case Tile::PATH_RIGHTLEFT:
                     std::cout << '-';
                     break;
+                case Tile::CITY:
+                    std::cout << 'H';
+                    break;
             }
         }
         std::cout << std::endl;
+    }
+}
+
+void Map::generateCities(int numCities) {
+    std::uniform_int_distribution<> disRow(0, m_Rows-1);
+    std::uniform_int_distribution<> disCol(0, m_Cols-1);
+
+    for (int i = 0; i < numCities; ++i) {
+        int x = disRow(gen);
+        int y = disCol(gen);
+        m_Grid[x][y] = Tile::CITY;
+        m_Cities.emplace_back(x,y);
+    }
+}
+void Map::generatePaths() {
+    for (const auto& start : m_Cities) {
+        for (const auto& end : m_Cities) {
+            if (start != end) {
+                dijkstraPath(start.first, start.second, end.first, end.second);
+            }
+        }
     }
 }
