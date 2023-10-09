f84ba5995f0432a2d089ff413f8bb3feb9581562
Daniel Adámek <daniel.adamek123@icloud.dom>
2023-10-09 15:11:04 +0200

    example map generation

--- main.cpp
+++ main.cpp
@@ -1,11 +1,22 @@
-#include "src/Map.h"
-#include "src/Tile.h"
-#include "src/Map.h"
+#include "src/CMap.h"
+#include "src/CTile.h"
+#include "src/CMap.h"
+#include "src/CTileConfig.h"
+#include "src/TileGenerator.h"
 
 int main() {
-    Map myMap(20, 20, Tile::GROUND);
-    myMap.generateCities(5);
-    myMap.generatePaths();
-    myMap.printMap();
+    CTileConfig tileList;
+
+    if (tileList.loadConfig("../tile_config.cfg")) {
+        std::cout << "Config file loaded successfully." << std::endl;
+
+    } else {
+        std::cerr << "Failed to load config file." << std::endl;
+    }
+    CMap map = CMap(10, 10);
+
+    TileGenerator generator = TileGenerator(map, tileList);
+    generator.generateTilesBFS();
+
     return 0;
 }
