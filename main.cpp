57d8e99f32c5dff495893930761fceea05001bba
Daniel Adámek <daniel.adamek123@icloud.dom>
2023-10-20 22:56:37 +0200

    main and cmakelist

--- main.cpp
+++ main.cpp
@@ -4,30 +4,33 @@
 #include "src/CTile.h"
 #include "src/CMap.h"
 #include "src/CTileConfig.h"
-#include "src/TileGenerator.h"
+#include "src/CTileGenerator.h"
 #include "opencv2/opencv.hpp"
 #include "src/CImageConnector.h"
+
+#define CONFIG "./tile_config_without_ends.cfg"
 
 int main() {
 
     CTileConfig tileConfig;
 
-    if (tileConfig.loadConfig("./tile_config.cfg"))
+    std::cout << "Loading config from..." << CONFIG << std::endl << std::endl;
+    if (tileConfig.loadConfig(CONFIG))
         std::cout << "Config file loaded successfully." << std::endl;
     else
         throw std::invalid_argument("Failed to load config file.");
 
     tileConfig.printLoadedTiles();
 
-    auto map = CMap(501, 501, tileConfig);
+    auto map = CMap(51, 51, tileConfig);
 
     std::vector<std::vector<bool>> visited(map.getRows(), std::vector<bool>(map.getCols(), false));
+    auto generator = CTileGenerator(map, tileConfig);
 
-    auto generator = TileGenerator(map, tileConfig);
+    generator.generateTilesDFS(0,0, visited);
+    //generator.generateTilesBFS();
 
-    generator.generateTilesDFS();
-    map.printMap();
-
+    std::cout << "Generating image..." << std::endl;
     auto ic = CImageConnector();
     ic.processMap(map, tileConfig.getAllTileConfig());
     ic.exportResult("./result.png");
